0478ed707523477ab7d48427d9231cc7835b0e13
luongductrong <luongductrong2004@gmail.com>
2026-03-23 22:46:41 +0700

    Use ODATA_SERVICE path for CSRF fetch
    Update fetchCsrfToken to request `${ODATA_SERVICE.ATTACHMENT}/$metadata` (removing the concatenation with ODATA_BASE_URL) and remove the now-unused ODATA_BASE_URL import. This relies on axiosInstance's baseURL for full URL resolution and cleans up the imports.

--- src/libs/helpers.ts
+++ src/libs/helpers.ts
@@ -1,12 +1,12 @@
+import { ODATA_SERVICE } from '@/app-constant';
 import { axiosInstance } from './axios-instance';
-import { ODATA_BASE_URL, ODATA_SERVICE } from '@/app-constant';
 
 export function getCsrfToken() {
   return sessionStorage.getItem('x-csrf-token');
 }
 
 export function fetchCsrfToken() {
-  return axiosInstance.get(`${ODATA_BASE_URL}${ODATA_SERVICE.ATTACHMENT}/$metadata`, {
+  return axiosInstance.get(`${ODATA_SERVICE.ATTACHMENT}/$metadata`, {
     headers: {
       'x-csrf-token': 'Fetch',
     },
